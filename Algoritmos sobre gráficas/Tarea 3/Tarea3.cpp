3e9638f5f4069e43eb50b794f546c26e4ef30682
OsmarSantana369 <89064313+OsmarSantana369@users.noreply.github.com>
2025-08-22 14:40:59 -0600

    Update Tarea3.cpp

--- Algoritmos sobre gráficas/Tarea 3/Tarea3.cpp
+++ Algoritmos sobre gráficas/Tarea 3/Tarea3.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
-#include <locale.h>
+##include <iostream>
+#include <locale>
 #include <vector>
+#include <cctype>
 using namespace std;
 
-// Osmar Dominique Santana Reyes
+/*
+Osmar Dominique Santana Reyes
 
-// Algoritmo de búsqueda en profundidad
-// Este programa obtiene el árbol generador de una gráfica dada (o bosque generador si la gráfica es disconexa).
-// Para esto, se obtendrá una lista de aristas que inducirán el árbol o bosque generador de la gráfica original.
-// La función Buscar() asigna al vector *visitado* tantos ceros según el orden de la gráfica, inicializa un índice *id* en 0 y verifica qué vértices de la gráfica ya han sido visitados, es decir,
-// cuáles ya han sido cubiertos por las aristas. Esto último es importante en el caso de las gráficas disconexas, pues en lo que sigue del algoritmo se obtendrán las aristas deseadas de solo una de las componentes conexas.
-// Si hay un vértice que no haya sido visitado entonces se usa la función Visitar(), con entrada el vértice no visitado. En caso contrario, no se hace nada.
-//     Luego, en esta función se aumenta en 1 la variable *id* y el valor de esta se le asigna al vector *visitado* en la entrada que corresponde al vértice no visitado y se imprime la etiqueta del vértice.
-//     Posteriormente, si el vértice no visitado es adyacente a otro vértice que tampoco ha sido visitado entonces ambos son registrados en el vector *Indaristas*,
-//     el cual contiene los pares de vértices de cada arista que será parte del árbol generador, además de que se volverá a ejecutar la función Visitar() pero cuya entrada es el otro vector no visitado.
-//     La función Visitar() se ejecutará hasta que haya un vértice que no sea adyacente a otro no visitado. Cuando esto pase, el programa,
-//     continuará con los vértices que se hayan quedado pendientes y terminará de ejecutarse cuando todos los vértices hayan sido visitados, es decir, cuando el vector *visitado* ya no tenga ceros.
-// Finalmente, se imprimen los vértices terminales de las aristas que inducen el árbol o bosque generador de la gráfica dada.
-// En caso, de haber vértices no cubiertos por estas aristas, entonces estos vértices son aislados.
+Este programa obtiene la matriz de adyacencia de una gráfica y la imprime.
 
-// Orden: O((Indaristas.size()+1)/2 + tamano + orden^2)
+Para esto se ingresa el orden y tamaño de la gráfica, en caso de no ser los adecuados, se imprime un mensaje con esta aclaración.
+Cuando sí lo son se crea una matriz cuadrada de dimensión *orden* y se ejecuta la función IngresaAristas() que solicita las adyacencias de la gráfica,
+a partir de las etiquetas de los vértices (las letras del abecedario iniciando en la "a").
+Luego, se obtiene su índice dentro de la matriz de adyacencia y se coloca 1 en las entradas correspondientes.
+Si algún vértice no corresponde a la gráfica se imprime un mensaje para volver a hacer la incersión.
+Después, se ejecuta la función ImprimeMatriz() que imprime la matriz junto con encabezados de los vértices que corresponden.
 
-void IngresaAristas(int tamano, int M[30][30]);
-void ImprimeMatriz(int orden, int M[30][30]);
-void Buscar(int visitado[30]);
+Orden del algoritmo: O(tamano + orden^2)
+*/
+
+void IngresaAristas(int tamano, vector<vector<int>>& M);
+void ImprimeMatriz(int orden, vector<vector<int>>& M);
+void Buscar(int vector<int>& visitado);
 void Visitar(int k, int id);
 
 std::vector<char> Indaristas;
@@ -34,7 +32,8 @@ int i, j, orden, tamano, M[30][30], visitado[30];
 int main()
 {
     setlocale(LC_ALL, "");
-    cout<<"Ingrese el orden de la gráfica: ";
+    cout
+    <<"Ingrese el orden de la gráfica: ";
     cin>>orden;
 
     cout<<endl<<"Ingrese el tamaño de la gráfica: ";
@@ -51,7 +50,7 @@ int main()
 
     cout<<endl<<endl;
 
-    cout<<"El árbol (o bosque) generador es la subgráfica inducida por las aristas: "<<endl;
+    cout<<"El árbol generador es la subgráfica inducida por las aristas: "<<endl;
 
     for(i = 0; i < (Indaristas.size()+1)/2; i++)
     {
@@ -61,50 +60,42 @@ int main()
     return 0;
 }
 
-
-void IngresaAristas(int tamano, int M[30][30])
+//Función para ingresar las adyacencias de la gráfica
+void IngresaAristas(int tamano, vector<vector<int>>& M)
 {
     char v1, v2;
-    for(i = 1; i <= tamano; i++)
-    {
-        cout<<endl<<"Inserte los vértices de la "<<i<<"° arista: ";
+    for (int i = 1; i <= tamano; i++) {
+        cout<<"\nInserte los vértices de la "<<i<<"° arista: ";
         cin>>v1>>v2;
 
-        int ver1 = v1;
-        int ver2 = v2;
-        ver1 = ver1 - 96;
-        ver2 = ver2 - 96;
+        int ver1 = tolower(v1) - 96;
+        int ver2 = tolower(v2) - 96;
 
-        M[ver1][ver2] = 1;
-        M[ver2][ver1] = 1;
+        if (ver1 > 0 && ver1 <= M.size() && ver2 > 0 && ver2 <= M.size()) {
+            M[ver1][ver2] = 1;
+            M[ver2][ver1] = 1;
+        } else {
+            cout<<"Vértices fuera de rango. Intentelo nuevamente.\n";
+            i--;
+        }
     }
 }
 
-void ImprimeMatriz(int orden, int M[30][30])
+// Función para imprimir la matriz de adyacencia
+void ImprimeMatriz(int orden, vector<vector<int>>& M)
 {
-    char letra1;
-    letra = 'a';
-    letra1 = letra;
-
-    cout<<" ";
-    for(i = 1; i <= orden; i++)
-    {
-        cout<<"   "<<letra;
-        letra++;
+    cout<<"\n ";
+    for (int i = 1; i <= orden; i++) {
+        cout<<"   "<<char(96 + i);
     }
 
-    for(i = 1; i <= orden; i++)
-    {
-        cout<<endl<<letra1<<"   ";
-        letra1++;
-
-        for(j = 1; j <= orden; j++)
-        {
+    for (int i = 1; i <= orden; i++) {
+        cout<<"\n"<<char(96 + i)<<"   ";
+        for (int j = 1; j <= orden; j++) {
             cout<<M[i][j]<<"   ";
         }
     }
-
-    cout<<endl<<endl;
+    cout<<endl;
 }
 
 void Buscar(int visitado[30])
