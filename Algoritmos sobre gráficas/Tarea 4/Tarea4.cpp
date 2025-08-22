3d5506728c08352a9194295192026eefec2e21b7
OsmarSantana369 <89064313+OsmarSantana369@users.noreply.github.com>
2025-08-22 14:41:24 -0600

    Update Tarea4.cpp

--- Algoritmos sobre gráficas/Tarea 4/Tarea4.cpp
+++ Algoritmos sobre gráficas/Tarea 4/Tarea4.cpp
@@ -1,81 +1,109 @@
 #include <iostream>
-#include<locale>
+#include <locale>
+#include <vector>
+#include <cctype>
 using namespace std;
 
-//Osmar Dominique Santana Reyes
-//Descrpcion
-//Orden
+/*
+Osmar Dominique Santana Reyes
 
-void IngresaAristas(int tamano, int M[30][30]);
-void ImprimeMatriz(int orden, int M[30][30]);
+Este programa verifica si una gráfica dada es completa a partir de su matriz de adyacencia
 
-char letra;
-int i, j, orden, tamano, M[30][30];
+Para esto se verifica si el *tamano* de la gráfica ingresada es igual a la mitad del producto del *orden* por este mismo disminuido en 1.
+Si lo es, se muestra un mensaje de que la gráfica es completa, en caso contrario el mensaje dirá que no lo es y se imprimirán las aristas que faltan para que lo sea.
 
+Orden del algoritmo: O(tamano + orden^2)
+*/
+
+void IngresaAristas(int tamano, vector<vector<int>>& M);
+void ImprimeMatriz(int orden, vector<vector<int>>& M);
+void EsCompleta(int orden, int tamano, vector<vector<int>>& M);
 
 int main()
 {
     setlocale(LC_ALL, "");
-    cout<<"Ingrese el orden de la gr�fica: ";
+    int orden, tamano;
+
+    cout<<"Ingrese el orden de la gráfica: ";
     cin>>orden;
 
-    cout<<endl<<"Ingrese el tama�o de la gr�fica: ";
+    if (orden <= 0) {
+        cout<<"Orden inválido. Debe ser mayor que 0.\n";
+        return 1;
+    }
+
+    cout<<"\nIngrese el tamaño de la gráfica: ";
     cin>>tamano;
 
+    if (tamano < 0 || tamano > orden*(orden - 1)/2) {
+        cout<<"Tamaño inválido para una gráfica simple.\n";
+        return 1;
+    }
+
+    vector<vector<int>> M(orden+1, vector<int>(orden+1, 0));
+
     IngresaAristas(tamano, M);
-
-    cout<<endl;
-
-    cout<<"La matriz de adyacencia es la siguiente:"<<endl;
     ImprimeMatriz(orden, M);
 
-
+    EsCompleta(orden, tamano, M);
 
     return 0;
 }
 
-
-void IngresaAristas(int tamano, int M[30][30])
+// Función para ingresar las adyacencias de la gráfica
+void IngresaAristas(int tamano, vector<vector<int>>& M)
 {
     char v1, v2;
-    for(i = 1; i <= tamano; i++)
-    {
-        cout<<endl<<"Inserte los v�rtices de la "<<i<<"� arista: ";
+    for (int i = 1; i <= tamano; i++) {
+        cout<<"\nInserte los vértices de la "<<i<<"° arista: ";
         cin>>v1>>v2;
 
-        int ver1 = v1;
-        int ver2 = v2;
-        ver1 = ver1 - 96;
-        ver2 = ver2 - 96;
+        int ver1 = tolower(v1) - 96;
+        int ver2 = tolower(v2) - 96;
 
-        M[ver1][ver2] = 1;
-        M[ver2][ver1] = 1;
+        if (ver1 > 0 && ver1 <= M.size() && ver2 > 0 && ver2 <= M.size()) {
+            M[ver1][ver2] = 1;
+            M[ver2][ver1] = 1;
+        } else {
+            cout<<"Vértices fuera de rango. Intentelo nuevamente.\n";
+            i--;
+        }
     }
 }
 
-void ImprimeMatriz(int orden, int M[30][30])
+// Función para imprimir la matriz de adyacencia
+void ImprimeMatriz(int orden, vector<vector<int>>& M)
 {
-    char letra1;
-    letra = 'a';
-    letra1 = letra;
-
-    cout<<" ";
-    for(i = 1; i <= orden; i++)
-    {
-        cout<<"   "<<letra;
-        letra++;
+    cout<<"\n ";
+    for (int i = 1; i <= orden; i++) {
+        cout<<"   "<<char(96 + i);
     }
 
-    for(i = 1; i <= orden; i++)
-    {
-        cout<<endl<<letra1<<"   ";
-        letra1++;
-
-        for(j = 1; j <= orden; j++)
-        {
+    for (int i = 1; i <= orden; i++) {
+        cout<<"\n"<<char(96 + i)<<"   ";
+        for (int j = 1; j <= orden; j++) {
             cout<<M[i][j]<<"   ";
         }
     }
-
-    cout<<endl<<endl;
+    cout<<endl;
 }
+
+// Función para ver si la gráfica es completa
+void EsCompleta(int orden, int tamano, vector<vector<int>>& M)
+{
+    if(tamano == orden*(orden-1)/2)
+        cout<<endl<<"La gráfica es completa"<<endl;
+    else{
+        cout<<endl<<"La gráfica no es completa"<<endl<<endl;
+        cout<<"Las aristas que faltan son:"<<endl;
+
+        for (int i = 1; i <= orden; i++) {
+            for (int j = i+1; j <= orden; j++) {
+                if(M[i][j] == 0)
+                    cout<<char(i+96)<<char(j+96)<<endl;
+            }
+        }
+    }
+}
+
+
