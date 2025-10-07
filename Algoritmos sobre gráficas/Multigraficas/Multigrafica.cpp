bcf6de438e8c490fa1706ee63e7f6ef8e22eb070
Osmar Dominique <osantanar001@alumno.uaemex.mx>
2025-10-07 01:23:45 -0600

    Multigraficas casi listo

--- Algoritmos sobre gráficas/Multigraficas/Multigrafica.cpp
+++ Algoritmos sobre gráficas/Multigraficas/Multigrafica.cpp
@@ -4,26 +4,42 @@
 using namespace std;
 
 struct Edge {
-    int from;
-    int to;
-    int color;
+    int v1;
+    int v2;
+    float weight;
+    vector<float> color;
 };
 
 void exportEdges(const vector<Edge>& edges, const string& filename) {
     ofstream file(filename);
-    file << "from,to,color\n";
+    file << "EndNodes_1,EndNodes_2,Weight,Color_R,Color_G,Color_B\n";
     for (const auto& edge : edges) {
-        file << edge.from << "," << edge.to << "," << edge.color << "\n";
+        file << edge.v1 << "," << edge.v2 << "," << edge.weight << ",";
+        for (const auto& c : edge.color) {
+            file << c << ",";
+        }
+        file << "\n";
     }
     file.close();
 }
 
 int main() {
     vector<Edge> edges = {
-        {1, 2, 0},
-        {2, 3, 1},
-        {3, 1, 2}
+        {1, 2, 6, {0, 0, 0}},
+        {1, 3, 6, {1, 0, 0}},
+        {1, 2, 7, {0, 1, 0}},
+        {2, 3, 11, {0, 0, 1}},
+        {3, 4, 17, {1, 1, 0}}
     };
     exportEdges(edges, "edges.csv");
+
+    vector<Edge> edges2 = {
+        {1, 2, 6, {0, 0, 0}},
+        {1, 3, 6, {1, 0, 0}},
+        {1, 2, 7, {0, 1, 0}},
+        {2, 3, 11, {0, 0, 1}},
+        {3, 2, 17, {1, 1, 0}}
+    };
+    exportEdges(edges2, "edges2.csv");
     return 0;
 }
