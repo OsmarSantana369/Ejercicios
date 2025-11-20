5ee5262402931aa19631a3a22c3bb0a346eeb2e9
Osmar Dominique <osantanar001@alumno.uaemex.mx>
2025-11-20 04:11:44 -0600

    Programa terminado

--- Algoritmos sobre gráficas/Tarea 26/Programa26.cpp
+++ Algoritmos sobre gráficas/Tarea 26/Programa26.cpp
@@ -23,7 +23,10 @@ struct Pareja{
 void IngresaPreferencias(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres);
 void ImprimeMatriz(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres);
 Pareja MaximaCorrespondencia(vector<Pareja>& parejasOptimas);
-vector<Pareja> EmparejamientoOptimo(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres);
+int BuscarEnColumna(vector<vector<int>>& matriz, int columna, int valorBuscado);
+void EliminarParejasRepetidas(vector<Pareja>& parejasOptimas);
+vector<Pareja> ObtenerParejasOptimas(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres, vector<bool>& mujeresEmparejadas, vector<bool>& hombresEmparejados);
+vector<Pareja> Emparejar(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres);
 
 int parejas;
 bool Reiniciar;
@@ -32,7 +35,7 @@ int main(){
     setlocale(LC_ALL, "");
 
     do{
-        cout << endl << "Ingrese la cantidad de parejas (el número de mujeres debe ser igual al número de hombres): ";
+        cout << "Ingrese la cantidad de parejas (el número de mujeres debe ser igual al número de hombres): ";
         cin >> parejas;
 
         if(parejas <= 0){
@@ -48,7 +51,7 @@ int main(){
 
     IngresaPreferencias(Mujeres, Hombres);
     ImprimeMatriz(Mujeres, Hombres);
-    vector<Pareja> resultado = EmparejamientoOptimo(Mujeres, Hombres);
+    vector<Pareja> resultado = Emparejar(Mujeres, Hombres);
 
     cout << endl << endl << "Emparejamientos óptimos encontrados:" << endl << endl;
     for(const auto& pareja : resultado){
@@ -153,7 +156,7 @@ int BuscarEnColumna(vector<vector<int>>& matriz, int columna, int valorBuscado){
 }
 
 // Función que elimina los elementos repetidos de un vector de parejas
-void EliminarRepetidos(vector<Pareja>& parejasOptimas){
+void EliminarParejasRepetidas(vector<Pareja>& parejasOptimas){
     for(int i = 0; i < parejasOptimas.size(); i++){
         for(int j = i + 1; j < parejasOptimas.size(); j++){
             if(parejasOptimas[i].mujer == parejasOptimas[j].mujer && parejasOptimas[i].hombre == parejasOptimas[j].hombre){
@@ -164,121 +167,136 @@ void EliminarRepetidos(vector<Pareja>& parejasOptimas){
     }
 }
 
-// Función que obtiene un vector con emparejamientos óptimos
-vector<Pareja> EmparejamientoOptimo(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres){
-    vector<Pareja> resultado;
-    vector<bool> mujeresEmparejadas(parejas + 1, false);
-    vector<bool> hombresEmparejados(parejas + 1, false);
-    Reiniciar = true;
+// Función que obtiene un vector con las parejas disponibles de mayor correspondencia
+vector<Pareja> ObtenerParejasOptimas(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres, vector<bool>& mujeresEmparejadas, vector<bool>& hombresEmparejados){
+    vector<Pareja> parejasOptimas;
 
-    do{
-        vector<Pareja> parejasOptimas;
+    // Mujeres eligen
+    for(int i = 1; i <= parejas; i++){
+        if(!mujeresEmparejadas[i]){
+            for(int j = 1; j <= parejas; j++){
+                int hombrePreferido = Mujeres[j][i];
 
-        // Mujeres eligen
-        for(int i = 1; i <= parejas; i++){
-            if(!mujeresEmparejadas[i]){
-                for(int j = 1; j <= parejas; j++){
-                    int hombrePreferido = Mujeres[i][j];
-
-                    if(!hombresEmparejados[hombrePreferido]){
-                        Pareja nuevaPareja;
-                        nuevaPareja.hombre = hombrePreferido;
-                        nuevaPareja.mujer = i;
-                        
-                        for(int k = 1; k <= parejas; k++){
-                            if(Hombres[hombrePreferido][k] == i){
-                                nuevaPareja.correspondencia = 2*(parejas + 1) - j - k;
-                                break;
-                            }
+                if(!hombresEmparejados[hombrePreferido]){
+                    Pareja nuevaPareja;
+                    nuevaPareja.hombre = hombrePreferido;
+                    nuevaPareja.mujer = i;
+                    
+                    for(int k = 1; k <= parejas; k++){
+                        if(Hombres[k][hombrePreferido] == i){
+                            nuevaPareja.correspondencia = 2*(parejas + 1) - j - k;
+                            break;
                         }
-                        parejasOptimas.push_back(nuevaPareja);
-                        break;
                     }
-                }
-            }
-        }
-        // Hombres eligen
-        for(int i = 1; i <= parejas; i++){
-            if(!hombresEmparejados[i]){
-                for(int j = 1; j <= parejas; j++){
-                    int mujerPreferida = Hombres[i][j];
-
-                    if(!mujeresEmparejadas[mujerPreferida]){
-                        Pareja nuevaPareja;
-                        nuevaPareja.hombre = i;
-                        nuevaPareja.mujer = mujerPreferida;
-                        
-                        for(int k = 1; k <= parejas; k++){
-                            if(Mujeres[mujerPreferida][k] == i){
-                                nuevaPareja.correspondencia = 2*(parejas + 1) - j - k;
-                                break;
-                            }
-                        }
-                        parejasOptimas.push_back(nuevaPareja);
-                        break;
-                    }
-                }
-            }
-        }
-        EliminarRepetidos(parejasOptimas);
-        
-        while(Reiniciar){
-            Pareja parejaSeleccionada = MaximaCorrespondencia(parejasOptimas);
-            mujeresEmparejadas[parejaSeleccionada.mujer] = true;
-            hombresEmparejados[parejaSeleccionada.hombre] = true;
-            resultado.push_back(parejaSeleccionada);
-
-            // Descartar la pareja seleccionada de las opciones
-            for(int i = 0; i < parejasOptimas.size(); i++){
-                if(parejasOptimas[i].mujer == parejaSeleccionada.mujer && parejasOptimas[i].hombre == parejaSeleccionada.hombre){
-                    parejasOptimas[i].correspondencia = -1; // Marcar como inválida
-                }
-            }
-            if(resultado.size() == 1)
-                continue;
-
-            for(int i = 0; i < resultado.size() - 1; i++){
-                if(resultado[i].mujer == parejaSeleccionada.mujer){
-                    int h1 = BuscarEnColumna(Mujeres, parejaSeleccionada.mujer, resultado[i].hombre);
-                    int h2 = BuscarEnColumna(Mujeres, parejaSeleccionada.mujer, parejaSeleccionada.hombre);
-
-                    if(h1 < h2){
-                        // La pareja anterior es mejor, deshacer el emparejamiento actual
-                        hombresEmparejados[parejaSeleccionada.hombre] = false;
-                        resultado.pop_back();
-                    } else{
-                        // La nueva pareja es mejor, deshacer el emparejamiento anterior
-                        hombresEmparejados[resultado[i].hombre] = false;
-                        resultado[i] = parejaSeleccionada;
-                        resultado.pop_back();
-                    }
-                }
-                if(resultado[i].hombre == parejaSeleccionada.hombre){
-                    int m1 = BuscarEnColumna(Hombres, parejaSeleccionada.hombre, resultado[i].mujer);
-                    int m2 = BuscarEnColumna(Hombres, parejaSeleccionada.hombre, parejaSeleccionada.mujer);
-
-                    if(m1 < m2){
-                        // La pareja anterior es mejor, deshacer el emparejamiento actual
-                        mujeresEmparejadas[parejaSeleccionada.mujer] = false;
-                        resultado.pop_back();
-                    } else{
-                        // La nueva pareja es mejor, deshacer el emparejamiento anterior
-                        mujeresEmparejadas[resultado[i].mujer] = false;
-                        resultado[i] = parejaSeleccionada;
-                        resultado.pop_back();
-                    }
-                }
-            }
-            Reiniciar = false;
-            
-            for(int i = 0; i < parejasOptimas.size(); i++){
-                if(parejasOptimas[i].correspondencia != -1 && resultado.size() < parejas){
-                    Reiniciar = true;
+                    parejasOptimas.push_back(nuevaPareja);
                     break;
                 }
             }
         }
-    } while(resultado.size() < parejas);
+    }
+    // Hombres eligen
+    for(int i = 1; i <= parejas; i++){
+        if(!hombresEmparejados[i]){
+            for(int j = 1; j <= parejas; j++){
+                int mujerPreferida = Hombres[j][i];
+
+                if(!mujeresEmparejadas[mujerPreferida]){
+                    Pareja nuevaPareja;
+                    nuevaPareja.hombre = i;
+                    nuevaPareja.mujer = mujerPreferida;
+                    
+                    for(int k = 1; k <= parejas; k++){
+                        if(Mujeres[k][mujerPreferida] == i){
+                            nuevaPareja.correspondencia = 2*(parejas + 1) - j - k;
+                            break;
+                        }
+                    }
+                    parejasOptimas.push_back(nuevaPareja);
+                    break;
+                }
+            }
+        }
+    }
+    return parejasOptimas;
+}
+
+// Función que obtiene un vector con emparejamientos
+vector<Pareja> Emparejar(vector<vector<int>>& Mujeres, vector<vector<int>>& Hombres){
+    vector<Pareja> resultado;
+    vector<bool> mujeresEmparejadas(parejas + 1, false);
+    vector<bool> hombresEmparejados(parejas + 1, false);
+
+    do{
+        vector<Pareja> parejasOptimas = ObtenerParejasOptimas(Mujeres, Hombres, mujeresEmparejadas, hombresEmparejados);
+        EliminarParejasRepetidas(parejasOptimas);
+        
+        do{
+            Pareja parejaSeleccionada = MaximaCorrespondencia(parejasOptimas);
+            bool agregarPareja = true;
+
+            if(resultado.size() == 0){
+                mujeresEmparejadas[parejaSeleccionada.mujer] = true;
+                hombresEmparejados[parejaSeleccionada.hombre] = true;
+                resultado.push_back(parejaSeleccionada);
+            }
+            for(int i = 0; i < resultado.size(); i++){
+                if(resultado[i].mujer == parejaSeleccionada.mujer){
+                    agregarPareja = false;
+
+                    if(resultado[i].correspondencia == parejaSeleccionada.correspondencia){
+                        int h1 = BuscarEnColumna(Mujeres, parejaSeleccionada.mujer, resultado[i].hombre);
+                        int h2 = BuscarEnColumna(Mujeres, parejaSeleccionada.mujer, parejaSeleccionada.hombre);
+
+                        if(h1 > h2){
+                            // La nueva pareja es mejor, deshacer el emparejamiento anterior
+                            hombresEmparejados[resultado[i].hombre] = false;
+                            hombresEmparejados[parejaSeleccionada.hombre] = true;
+                            resultado[i] = parejaSeleccionada;
+                        }
+                    }
+                }
+                if(resultado[i].hombre == parejaSeleccionada.hombre){
+                    agregarPareja = false;
+
+                    if(resultado[i].correspondencia == parejaSeleccionada.correspondencia){
+                        int m1 = BuscarEnColumna(Hombres, parejaSeleccionada.hombre, resultado[i].mujer);
+                        int m2 = BuscarEnColumna(Hombres, parejaSeleccionada.hombre, parejaSeleccionada.mujer);
+
+                        if(m1 > m2){
+                            // La nueva pareja es mejor, deshacer el emparejamiento anterior
+                            mujeresEmparejadas[resultado[i].mujer] = false;
+                            mujeresEmparejadas[parejaSeleccionada.mujer] = true;
+                            resultado[i] = parejaSeleccionada;
+                        }
+                    }
+                }
+            }
+            if(agregarPareja){
+                mujeresEmparejadas[parejaSeleccionada.mujer] = true;
+                hombresEmparejados[parejaSeleccionada.hombre] = true;
+                resultado.push_back(parejaSeleccionada);
+            }
+            // Descartar la pareja seleccionada de las opciones
+            for(int i = 0; i < parejasOptimas.size(); i++){
+                if(parejasOptimas[i].mujer == parejaSeleccionada.mujer && parejasOptimas[i].hombre == parejaSeleccionada.hombre){
+                    parejasOptimas.erase(parejasOptimas.begin() + i);
+                    break;
+                }
+            }
+        } while(!parejasOptimas.empty() && resultado.size() < parejas - 1);
+    } while(resultado.size() < parejas - 1);
     
+    int sumaMujer = 0, sumaHombre = 0;
+
+    for(int i = 0; i < resultado.size(); i++){
+        sumaMujer += resultado[i].mujer;
+        sumaHombre += resultado[i].hombre;
+    }
+    Pareja ultimaPareja;
+    ultimaPareja.mujer = (parejas * (parejas + 1)) / 2 - sumaMujer;
+    ultimaPareja.hombre = (parejas * (parejas + 1)) / 2 - sumaHombre;
+    ultimaPareja.correspondencia = 2 * (parejas + 1) - BuscarEnColumna(Mujeres, ultimaPareja.mujer, ultimaPareja.hombre) - BuscarEnColumna(Hombres, ultimaPareja.hombre, ultimaPareja.mujer);
+    resultado.push_back(ultimaPareja);
+
     return resultado;
 }
